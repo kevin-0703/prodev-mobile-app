0489c1fa552e343878a85575a11c06b50550f13b
KEVIN NSHUTI <nkevin881@gmail.com>
2025-05-30 11:59:22 +0200

    adding files

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10;
+number2 = 5;
+addition = number1 + number2;
+substarction = number1 + number2;
+multiplication = number1 * number2;
+print('addition of 10 and 5 is', addition);
+print('subtraction of 10 and 5 is', substarction);
+print('multiplication of 10 and 5 is', multiplication);
