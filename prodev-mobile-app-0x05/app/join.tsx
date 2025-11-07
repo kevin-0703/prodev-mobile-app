4a877913cb87ee4b33cd36ff84bbaa85ca8c6190
KEVIN NSHUTI <nkevin881@gmail.com>
2025-11-07 19:10:58 +0200

    styling with nativewind and react native UI implementation challenge

---
+++ prodev-mobile-app-0x05/app/join.tsx
@@ -0,0 +1,56 @@
+import { Text, TextInput, View, TouchableOpacity, Image } from "react-native";
+import { styles } from "@/styles/_join";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+
+export default function Index() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.iconsection}>
+          <Ionicons name="arrow-back" size={25} />
+          <Image source={require("@/assets/images/logoc.png")} />
+        </View>
+        <view style={styles.titleTextGroup}>
+          <Text style={styles.titleText}>Join Us</Text>
+          <Text style={styles.subText}>
+            Enter your email and password to sign in.
+          </Text>
+        </view>
+
+        <View style={styles.formGroup}>
+          <View>
+            <Text style={styles.formLabel}>Email</Text>
+            <TextInput
+              keyboardType="email-address"
+              style={styles.formControl}
+            />
+          </View>
+          <View style={{ marginTop: 20 }}>
+            <Text style={styles.formLabel}>Password</Text>
+            <View style={styles.formPasswordControl}>
+              <TextInput style={styles.passwordControl} />
+              <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
+            </View>
+          </View>
+          <Text style={styles.forgotPasswordText}>Forgot password?</Text>
+        </View>
+
+        <TouchableOpacity style={styles.primaryButton}>
+          <Text style={styles.buttonText}>Join Us</Text>
+        </TouchableOpacity>
+
+        <View style={styles.dividerGroup}>
+          <View style={styles.divider}></View>
+          <Text style={styles.dividerText}>OR</Text>
+          <View style={styles.divider}></View>
+        </View>
+        <view style={styles.secondaryButtonGroup}>
+          <TouchableOpacity style={styles.secondaryButton}>
+            <Text style={styles.secondaryButtonText}>Sign in</Text>
+          </TouchableOpacity>
+        </view>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
